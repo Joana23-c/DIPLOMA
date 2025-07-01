f7328dac30775a7b84d1a9e99fe46a31303fabc4
Joana Cullhaj <joanacullhaj@gmail.com>
2025-07-01 04:18:52 +0200

    gjera te vogle

--- Sistemi Diellor/script.js
+++ Sistemi Diellor/script.js
@@ -6,12 +6,10 @@ function createStars(){
     stars.className="stars";
     stars.style.width = ".1px";/* madhesia: 0.1px × 0.1px */
     stars.style.height=".1px";
-    /*i vendos ne pozicione random
-    nese i fshij keto te dyja nuk do te vendosen yjet random edhe do te gjenerohen te gjitha parane njeratjetres duke krijuar nje vize ne mes te body sepse e kam cilesuar qe cdo element i body duhet te vendoset ne mes te tij */
     stars.style.top = Math.random() * 100 + "%";
     stars.style.left = Math.random() * 100 + "%";
     
-    container.appendChild(stars);/*e bej visible */
+    container.appendChild(stars);
   }
 }
 createStars();
